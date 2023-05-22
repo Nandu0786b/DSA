05c113f3bfcf3a8489321aacad441ed9bc5b11c7
Nand Kishore Ola <nandkishoreola0786b@gmail.com>
2023-05-22 06:05:08 +0530

    Remove Duplicates From Sorted List

---
+++ Linked list/48_1_Remove_Duplicates_From_Sorted_List.cpp
@@ -0,0 +1,38 @@
+// https://www.codingninjas.com/codestudio/problems/unique-sorted-list_2420283?leftPanelTab=1
+
+/************************************************************
+
+    Following is the linked list node structure.
+    
+    class Node 
+    {
+        public:
+        int data;
+        Node* next;
+
+        Node(int data) 
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+    
+************************************************************/
+
+Node * uniqueSortedList(Node * head) {
+    if (head==NULL){
+        return NULL;
+    }
+    Node* current =head;
+    while(current!=NULL){
+        if((current->next!=NULL) && (current->data==current->next->data)){
+            Node* duplicate=current->next;
+            current->next=current->next->next;
+            delete(duplicate);
+        }
+        else{
+            current=current->next;
+        }
+    }
+    return head;
+}
