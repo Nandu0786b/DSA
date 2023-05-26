3cd72332b5d4cca8799ab3526a7649a24a159fb5
Nand Kishore Ola <nandkishoreola0786b@gmail.com>
2023-05-26 10:27:09 +0530

    Insert An Element At Its Bottom In A Given Stack

---
+++ Stack/55_3_Insert_An_Element_At_Its_Bottom_In_A_Given_Stack.cpp
@@ -0,0 +1,22 @@
+// Insert An Element At Its Bottom In A Given Stack
+// https://www.codingninjas.com/codestudio/problems/insert-an-element-at-its-bottom-in-a-given-stack_1171166?leftPanelTab=0
+
+// very simple pop kartha javo and jab empty ho jaya tho element push kar do and fir backtracking sa sara ka sara element fir sa add kar do
+
+#include <bits/stdc++.h> 
+void solve(stack<int>& myStack, int x){
+    if(myStack.empty()){
+        myStack.push(x);
+        return;
+    }
+    int pop=myStack.top();
+    myStack.pop();
+    solve( myStack,x);
+    myStack.push(pop);
+}
+stack<int> pushAtBottom(stack<int>& myStack, int x) 
+{
+    solve( myStack,x);
+    return myStack;
+}
+
