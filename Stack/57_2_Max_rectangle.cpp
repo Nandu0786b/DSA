e402a490c8813e596d61e9c8589bbde9f6f3a852
Nand Kishore Ola <nandkishoreola0786b@gmail.com>
2023-05-27 23:46:08 +0530

    Max rectangle

---
+++ Stack/57_2_Max_rectangle.cpp
@@ -0,0 +1,72 @@
+// Max rectangle
+// https://practice.geeksforgeeks.org/problems/max-rectangle/1
+
+// very simple previous 56 wali max ractangle wali approach, baically start a loop for the every row in i=0 send only 0th row and check the maximum 
+// histogram area and store the maximum area , now next time send previous row and current row with the column wise sum and if in the current row if any 
+// element then that column set completely zero
+// finaly return the maxarea
+
+
+class Solution{
+    private:
+    vector<int> nextsmallerElement(int *arr, int n){
+        stack<int>s;
+        s.push(-1);
+        vector<int>ans(n);
+        for(int i=n-1;i>=0;i--){
+            int curr=arr[i];
+            while(s.top()!=-1 &&  arr[s.top()]>=curr){
+                s.pop();
+            }
+            ans[i]=s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    vector<int> prevsmallerElement(int *arr, int n){
+        stack<int>s;
+        s.push(-1);
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            int curr=arr[i];
+            while(s.top()!=-1 &&  arr[s.top()]>=curr){
+                s.pop();
+            }
+            ans[i]=s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    int largestRectangleArea(int *heights, int n) {
+
+       vector<int> next(n);
+       next=nextsmallerElement(heights,n);
+       vector<int> prev(n);
+       prev=prevsmallerElement(heights,n);
+       int area=INT_MIN;
+       for(int i=0;i<n;i++){
+           int l=heights[i];
+          
+           if(next[i]==-1){
+               next[i]=n;
+           }
+            int b=next[i]-prev[i]-1;
+           int newArea = l*b;
+           area=max(area,newArea);
+       }
+       return area;
+    }
+  public:
+    int maxArea(int M[MAX][MAX], int n, int m) {
+        // Your code here
+        int area=largestRectangleArea(M[0], m);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(M[i][j]!=0){
+                M[i][j]=M[i][j]+M[i-1][j];}
+            }
+            area=max(area,largestRectangleArea(M[i], m));
+        }
+        return area;
+    }
+};
