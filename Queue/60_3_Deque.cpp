b6816d75f873f4cd8e5e38cb211153354ee18293
Nand Kishore Ola <nandkishoreola0786b@gmail.com>
2023-05-29 18:28:52 +0530

    Implement Deque

---
+++ Queue/60_3_Deque.cpp
@@ -0,0 +1,140 @@
+// Implement Deque
+// https://www.codingninjas.com/codestudio/problems/deque_1170059?leftPanelTab=0
+
+#include <bits/stdc++.h> 
+class Deque
+{
+    int* arr;
+    int front;
+    int rear;
+    int size;
+public:
+    // Initialize your data structure.
+    Deque(int n)
+    {
+        // Write your code here.
+        this->size=n;
+        front=rear=-1;
+        arr=new int[n];
+    }
+
+    // Pushes 'X' in the front of the deque. Returns true if it gets pushed into the deque, and false otherwise.
+    bool pushFront(int x)
+    {
+        // Write your code here.
+        if((front==0 && rear==size-1) || (rear+1==front)){
+            return false;        }
+        else if(front==-1){
+            front=rear=0;
+        }
+        else if(front==0){
+            front=size-1;
+        }
+        else{
+            front--;
+        }
+        arr[front]=x;
+        return true;
+    }
+
+    // Pushes 'X' in the back of the deque. Returns true if it gets pushed into the deque, and false otherwise.
+    bool pushRear(int x)
+    {
+        // Write your code here.
+        if((front==0 && rear==size-1)||(rear+1==front)){
+            return false;        }
+        else if(front==-1){
+            front=rear=0;
+        }
+        else if(rear==size-1){
+            rear=0;
+        }
+        else{
+            rear++;
+        }
+        arr[rear]=x;
+        return true;
+    }
+
+    // Pops an element from the front of the deque. Returns -1 if the deque is empty, otherwise returns the popped element.
+    int popFront()
+    {
+        // Write your code here.
+         if(front==-1){//To check circular queue is empty
+            return -1;
+        }
+        int ans=arr[front];
+        arr[front]=-1;
+        if(front==rear){ //single lement present in the queue
+            front=rear=-1;
+        }
+        else if(front==size-1){//front is on the last element
+            front=0;
+        }
+        else{
+            front++;
+        }
+        return ans;
+    }
+
+    // Pops an element from the back of the deque. Returns -1 if the deque is empty, otherwise returns the popped element.
+    int popRear()
+    {
+        // Write your code here.
+         if(front==-1){//To check circular queue is empty
+            return -1;
+        }
+        int ans=arr[rear];
+        arr[rear]=-1;
+        if(front==rear){ //single lement present in the queue
+            front=rear=-1;
+        }
+        else if(rear==0){//front is on the last element
+            rear=size-1;
+        }
+        else{
+            rear--;
+        }
+        return ans;
+    }
+
+    // Returns the first element of the deque. If the deque is empty, it returns -1.
+    int getFront()
+    {
+        // Write your code here.
+        if(front==-1){
+            return -1;
+        }
+        return arr[front];
+    }
+
+    // Returns the last element of the deque. If the deque is empty, it returns -1.
+    int getRear()
+    {
+        // Write your code here.
+        if(front==-1){
+            return -1;
+        }
+        return arr[rear];
+    }
+
+    // Returns true if the deque is empty. Otherwise returns false.
+    bool isEmpty()
+    {
+        // Write your code here.
+        if(front==-1){
+            return true;
+        }
+        return false;
+    }
+
+    // Returns true if the deque is full. Otherwise returns false.
+    bool isFull()
+    {
+        // Write your code here.
+        if((front==0 && rear==size-1) || (front==rear+1)){
+            return true;
+        }
+        return false;
+    }
+};
