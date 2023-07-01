26e3d4987261ff882b4ce2096a0e07f552791833
Nand Kishore Ola <nandkishoreola0786b@gmail.com>
2023-07-01 15:33:16 +0530

    Russian Doll Envelopes

---
+++ DP/119_2_Russian Doll Envelopes.cpp
@@ -0,0 +1,9 @@
+// Russian Doll Envelopes
+
+
+// https://leetcode.com/problems/russian-doll-envelopes/
+
+//approach same as previous bst algorithm
+
+//but in this first we will sort weidth and if same width come then we will sort height in the decreasing order
+//  for revision
